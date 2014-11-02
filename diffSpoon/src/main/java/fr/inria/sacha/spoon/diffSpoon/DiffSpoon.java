55de83bb557788647894b5af56bced69847984af
Matias <matias.martinez@inria.fr>
2014-11-02 14:40:49 +0100

    Changing the generation of the model Writing Test Cases

--- diffSpoon/src/main/java/fr/inria/sacha/spoon/diffSpoon/DiffSpoon.java
+++ diffSpoon/src/main/java/fr/inria/sacha/spoon/diffSpoon/DiffSpoon.java
@@ -1,5 +1,10 @@
 package fr.inria.sacha.spoon.diffSpoon;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -9,6 +14,8 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import spoon.compiler.SpoonCompiler;
+import spoon.compiler.SpoonResource;
+import spoon.compiler.SpoonResourceHelper;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtSimpleType;
@@ -16,6 +23,7 @@ import spoon.reflect.factory.Factory;
 import spoon.reflect.factory.FactoryImpl;
 import spoon.support.DefaultCoreFactory;
 import spoon.support.StandardEnvironment;
+import spoon.support.compiler.jdt.JDTBasedSpoonCompiler;
 import spoon.support.compiler.jdt.JDTSnippetCompiler;
 import fr.labri.gumtree.Mapping;
 import fr.labri.gumtree.Mappings;
@@ -33,40 +41,72 @@ import fr.labri.gumtree.matchers.Matcher;
  * 
  */
 public class DiffSpoon {
-	
+
 	public static final Logger logger = Logger.getLogger(DiffSpoon.class);
 	protected Factory factory = null;
+
+	protected Set<Mapping> mappings = null;
+	protected Mappings mappingsComp = null;
 	
-	public DiffSpoon(Factory factory){
+	public DiffSpoon(Factory factory) {
 		this.factory = factory;
 	}
-	
-	public DiffSpoon(){
-		factory = new FactoryImpl(new DefaultCoreFactory(), new StandardEnvironment());
+
+	public DiffSpoon() {
+		factory = new FactoryImpl(new DefaultCoreFactory(),
+				new StandardEnvironment());
 		logger.setLevel(Level.DEBUG);
 	}
-	
-	public DiffSpoon(boolean noClasspath){
+
+	public DiffSpoon(boolean noClasspath) {
 		this();
 		factory.getEnvironment().setNoClasspath(noClasspath);
 	}
-	
+
 	public CtDiff analyze(String left, String right) {
 
-		CtClass<?> clazz1 = factory.Code().createCodeSnippetStatement(left).compile();
+		CtClass<?> clazz1 = factory.Code().createCodeSnippetStatement(left)
+				.compile();
 
-		CtClass<?> clazz2 = factory.Code().createCodeSnippetStatement(right).compile();
+		CtClass<?> clazz2 = factory.Code().createCodeSnippetStatement(right)
+				.compile();
 
+	
 		
 		return analyze(clazz1, clazz2);
 	}
+
+	public CtDiff analyze(URL f1, URL f2) throws Exception {
+		return this.analyze(new File(f1.getFile()), new File(f1.getFile()));
+	}
 	
-	Set<Mapping> mappings = null;
-	Mappings mappingsComp = null;
-	
-	
-	public CtDiff analyze(CtElement left, CtElement right) {
+	public CtDiff analyze(File f1, File f2) throws Exception {
+	//String content1 = readFile(f1);
+	//	String content2 = readFile(f2);
+	//	CtDiff result = this.analyze(content1, content2);
+		 
+		CtClass<?> clazz1 = getCtClass(f1);
+			
+		CtClass<?> clazz2 = getCtClass(f2);
 		
+		CtDiff result = this.analyze(clazz1,clazz2);
+		
+		return result;
+	}
+
+	protected CtClass getCtClass(File f) throws Exception{
+		SpoonResource sr1 = SpoonResourceHelper .createResource(f) ;
+		SpoonCompiler compiler = new JDTBasedSpoonCompiler(factory);
+		compiler.addInputSource(sr1);
+		compiler.build();
+		CtClass<?> clazz1 = (CtClass<?>) factory.Type().getAll().get(0);
+		factory.Package().getAllRoots().clear();
+		return clazz1;
+	}
+
+
+	public CtDiff analyze(CtElement left, CtElement right) {
+
 		SpoonGumTreeBuilder scanner = new SpoonGumTreeBuilder();
 
 		scanner.scan(left);
@@ -86,102 +126,124 @@ public class DiffSpoon {
 		ProduceFileTree gen = null;
 		gen = new ProduceJDTTree();
 
-		
 		List<Action> actions = null;
 
 		GumTreeMatcher.prepare(rootSpoonLeft);
 		GumTreeMatcher.prepare(rootSpoonRight);
-		
-		logger.debug("left tree:"+ rootSpoonLeft.toTreeString());
-		logger.debug("right tree:"+rootSpoonRight.toTreeString());
-	
-		//--
+
+		logger.debug("left tree:  " + rootSpoonLeft.toTreeString());
+		logger.debug("right tree: " + rootSpoonRight.toTreeString());
+
+		// --
 		Matcher matcher = new GumTreeMatcher(rootSpoonLeft, rootSpoonRight);
 		mappings = matcher.getMappings();
 		mappingsComp = new Mappings(mappings);
 
-		GenerateActions gt = new GenerateActions(rootSpoonLeft, rootSpoonRight, matcher.getMappings());
+		GenerateActions gt = new GenerateActions(rootSpoonLeft, rootSpoonRight,
+				matcher.getMappings());
 		actions = gt.getActions();
 
 		GTFacade gtfac = new GTFacade();
 		List<Action> rootActions = gtfac.getRootActions(mappings, actions);
-		logger.debug("Root Actions "+rootActions.size() );
+		logger.debug("Root Actions " + rootActions.size());
 		for (Action action : rootActions) {
 			logger.debug("--> " + action);
 		}
-		
-		logger.debug("All actions: "+actions.size());
+
+		logger.debug("All actions: " + actions.size());
 		for (Action action : actions) {
-			if(action.getNode().getParent() != null)
+			if (action.getNode().getParent() != null)
 				logger.debug("--> " + action);
 		}
 
-	
-		return new CtDiff(actions,rootActions);
+		return new CtDiff(actions, rootActions);
 	}
-	
+
 	/**
 	 * 
 	 * @param rootActions
 	 * @param actionParent
 	 * @return
 	 */
-	public List<Action> retriveActionChilds(List<Action> rootActions, Action actionParent) {
-		
+	public List<Action> retriveActionChilds(List<Action> rootActions,
+			Action actionParent) {
+
 		List<Action> actions = new ArrayList<Action>();
-		
+
 		for (Action action : rootActions) {
 			Tree t = action.getNode();
-			if(t.getParent().equals(actionParent)){
+			if (t.getParent().equals(actionParent)) {
 				actions.add(action);
 			}
-			
+
 		}
 
 		return rootActions;
 	}
-	public void getCtClass(Factory factory, String contents){
+
+	public void getCtClass(Factory factory, String contents) {
 		SpoonCompiler builder = new JDTSnippetCompiler(factory, contents);
 		try {
-			builder.build(); 
+			builder.build();
 		} catch (Exception e) {
 			throw new RuntimeException(
 					"snippet compilation error while compiling: " + contents, e);
 		}
 	}
 
-	
-	public CtClass getCtClass(String contents){
-		try{
+	public CtClass getCtClass(String contents) {
+		try {
 			this.getCtClass(factory, contents);
-		}catch(Exception e){
-			//must fails
+		} catch (Exception e) {
+			// must fails
 		}
 		List<CtSimpleType<?>> types = factory.Type().getAll();
 		CtSimpleType spt = types.get(0);
 		spt.getPackage().getTypes().remove(spt);
-		
+
 		return (CtClass) spt;
-		
+
 	}
-	
-	public String printTree(String tab,Tree t){
-	
-			StringBuffer b = new StringBuffer();
-			b.append(t.getTypeLabel()+":"+ t.getLabel()+ " \n");
-			Iterator<Tree> cIt = t.getChildren().iterator();
-			while (cIt.hasNext()) {
-				Tree c = cIt.next();
-				b.append(tab + " "+printTree("\t"+tab, c));
-			//	if (cIt.hasNext()) b.append(" ");
-			}
-			//b.append(")");
-			return b.toString();
-		
+
+	public String printTree(String tab, Tree t) {
+
+		StringBuffer b = new StringBuffer();
+		b.append(t.getTypeLabel() + ":" + t.getLabel() + " \n");
+		Iterator<Tree> cIt = t.getChildren().iterator();
+		while (cIt.hasNext()) {
+			Tree c = cIt.next();
+			b.append(tab + " " + printTree("\t" + tab, c));
+			// if (cIt.hasNext()) b.append(" ");
+		}
+		// b.append(")");
+		return b.toString();
+
 	}
+
+	public static void main(String[] args) throws Exception {
+
+		if (args.length != 2) {
+			System.out.println("Usage: DiffSpoon <file_1>  <file_2>");
+			return;
+		}
+
+		File f1 = new File(args[0]);
+		File f2 = new File(args[2]);
+
 	
-	public static void main(String[] args){
-		//TODO
+
+		DiffSpoon ds = new DiffSpoon(true);
+		CtDiff result = ds.analyze(f1, f2);
+
 	}
-	
+
+	private static String readFile(File f) throws IOException {
+		FileReader reader = new FileReader(f);
+		char[] chars = new char[(int) f.length()];
+		reader.read(chars);
+		String content = new String(chars);
+		reader.close();
+		return content;
+	}
+
 }
--- diffSpoon/src/test/java/fr/inria/sacha/spoon/diffSpoon/DiffSpoonTest.java
+++ diffSpoon/src/test/java/fr/inria/sacha/spoon/diffSpoon/DiffSpoonTest.java
@@ -1,8 +1,14 @@
 package fr.inria.sacha.spoon.diffSpoon;
 
+
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.List;
+
 import org.junit.Test;
+
+import fr.labri.gumtree.actions.Action;
 /**
  * 
  * @author  Matias Martinez, matias.martinez@inria.fr
@@ -29,4 +35,81 @@ public class DiffSpoonTest {
 		//fail("Not yet implemented");
 	}
 
+	@Test
+	public void exampleInsertAndUpdate() throws Exception{
+		
+		DiffSpoon diff = new DiffSpoon(true);
+		File fl = new File(getClass().
+				getResource("/examples/test1/TypeHandler1.java").getFile());
+		File fr = new File(getClass().
+				getResource("/examples/test1/TypeHandler2.java").getFile());
+	
+		CtDiff result = diff.analyze(fl,fr);
+		List<Action> actions = result.getRootActions();
+		assertEquals(actions.size(), 2);
+		assertTrue(containsAction(actions, "INS", "CtInvocationImpl"));
+		assertFalse(containsAction(actions, "DEL", "CtInvocationImpl"));
+		assertFalse(containsAction(actions, "UPD", "CtInvocationImpl"));
+		
+		assertTrue(containsAction(actions, "UPD", "CtFieldAccessImpl"));
+	}
+	
+	
+	@Test
+	public void exampleSingleUpdate() throws Exception{
+		
+		DiffSpoon diff = new DiffSpoon(true);
+		File fl = new File(getClass().
+				getResource("/examples/test2/CommandLine1.java").getFile());
+		File fr = new File(getClass().
+				getResource("/examples/test2/CommandLine2.java").getFile());
+	
+		CtDiff result = diff.analyze(fl,fr);
+		List<Action> actions = result.getRootActions();
+
+		assertTrue(containsAction(actions, "UPD", "PAR-CtLiteralImpl"));
+		assertEquals(actions.size(), 1);
+	}
+	
+	@Test
+	public void exampleRemoveMethod() throws Exception{
+		
+		DiffSpoon diff = new DiffSpoon(true);
+		File fl = new File(getClass().
+				getResource("/examples/test3/CommandLine1.java").getFile());
+		File fr = new File(getClass().
+				getResource("/examples/test3/CommandLine2.java").getFile());
+	
+		CtDiff result = diff.analyze(fl,fr);
+		List<Action> actions = result.getRootActions();
+		assertEquals(actions.size(), 1);
+		assertTrue(containsAction(actions, "DEL", "CtMethodImpl"));
+	}
+	
+	
+	@Test
+	public void exampleInsert() throws Exception{
+		
+		DiffSpoon diff = new DiffSpoon(true);
+		File fl = new File(getClass().
+				getResource("/examples/test4/CommandLine1.java").getFile());
+		File fr = new File(getClass().
+				getResource("/examples/test4/CommandLine2.java").getFile());
+	
+		CtDiff result = diff.analyze(fl,fr);
+		List<Action> actions = result.getRootActions();
+		assertEquals(actions.size(), 1);
+		assertTrue(containsAction(actions, "INS", "CtMethodImpl"));
+	}
+	private boolean containsAction(List<Action> actions, String kindAction, String kindNode){
+		//TODO: the kind of the action is not visible, To see in the next version of GumTree
+		for (Action action : actions) {
+			String toSt = action.toString();
+			if(toSt.startsWith(kindAction)){
+				return action.getNode().getTypeLabel().endsWith(kindNode);
+				//return true;
+			}
+		}
+		return false;
+	}
 }
