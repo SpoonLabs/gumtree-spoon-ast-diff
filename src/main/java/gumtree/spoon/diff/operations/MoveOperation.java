40023637310f929569387446442a3b7d3c51031a
tdurieux <durieuxthomas@hotmail.com>
2018-01-10 10:37:05 +0100

    destination of moved operation is not null

--- src/main/java/gumtree/spoon/diff/operations/MoveOperation.java
+++ src/main/java/gumtree/spoon/diff/operations/MoveOperation.java
@@ -1,9 +1,15 @@
 package gumtree.spoon.diff.operations;
 
 import com.github.gumtreediff.actions.model.Move;
+import spoon.reflect.declaration.CtElement;
 
 public class MoveOperation extends AdditionOperation<Move> {
 	public MoveOperation(Move action) {
 		super(action);
+	}
+
+	@Override
+	public CtElement getDstNode() {
+		return (CtElement) getAction().getNode().getMetadata("spoon_object_dest");
 	}
 }
