5ec7e55df80b0817d5fd226a2994b84cb927600b
Thomas Durieux <tdurieux@users.noreply.github.com>
2018-01-12 12:06:51 +0100

    fix: improve toString for debug (#38)

--- src/main/java/gumtree/spoon/diff/operations/Operation.java
+++ src/main/java/gumtree/spoon/diff/operations/Operation.java
@@ -1,8 +1,14 @@
 package gumtree.spoon.diff.operations;
 
 import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.actions.model.Move;
+import com.github.gumtreediff.actions.model.Update;
 import gumtree.spoon.builder.SpoonGumTreeBuilder;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtPackage;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.visitor.DefaultJavaPrettyPrinter;
 
 public abstract class Operation<T extends Action> {
 	private final CtElement node;
@@ -25,7 +31,71 @@ public abstract class Operation<T extends Action> {
 
 	@Override
 	public String toString() {
-		return action.toString();
+		return toStringAction(action);
+	}
+
+	private String toStringAction(Action action) {
+		String newline = System.getProperty("line.separator");
+		StringBuilder stringBuilder = new StringBuilder();
+
+
+		CtElement element = (CtElement) action.getNode().getMetadata(SpoonGumTreeBuilder.SPOON_OBJECT);
+		// action name
+		stringBuilder.append(action.getClass().getSimpleName());
+
+		// node type
+		String nodeType = element.getClass().getSimpleName();
+		nodeType = nodeType.substring(2, nodeType.length() - 4);
+		stringBuilder.append(" ").append(nodeType);
+
+		// action position
+		CtElement parent = element;
+		while (parent.getParent() != null && !(parent.getParent() instanceof CtPackage)) {
+			parent = parent.getParent();
+		}
+		String position = " at ";
+		if (parent instanceof CtType) {
+			position += ((CtType) parent).getQualifiedName();
+		}
+		if (element.getPosition() != null) {
+			position += ":" + element.getPosition().getLine();
+		}
+		if (action instanceof Move) {
+			CtElement elementDest = (CtElement) action.getNode().getMetadata(SpoonGumTreeBuilder.SPOON_OBJECT_DEST);
+			position = " from " + element.getParent(CtClass.class).getQualifiedName() + ":" + element.getPosition().getLine();
+			position += " to " + elementDest.getParent(CtClass.class).getQualifiedName() + ":" + elementDest.getPosition().getLine();
+		}
+		stringBuilder.append(position).append(newline);
+
+		// code change
+		String label = partialElementPrint(element);
+		if (action instanceof Move) {
+			label = element.toString();
+		}
+		if (action instanceof Update) {
+			CtElement elementDest = (CtElement) action.getNode().getMetadata(SpoonGumTreeBuilder.SPOON_OBJECT_DEST);
+			label += " to " + elementDest.toString();
+		}
+		String[] split = label.split(newline);
+		for (String s : split) {
+			stringBuilder.append("\t").append(s).append(newline);
+		}
+		return stringBuilder.toString();
+	}
+
+	private String partialElementPrint(CtElement element) {
+		DefaultJavaPrettyPrinter print = new DefaultJavaPrettyPrinter(element.getFactory().getEnvironment()) {
+			@Override
+			public DefaultJavaPrettyPrinter scan(CtElement e) {
+				if (e != null && e.getMetadata("isMoved") == null) {
+					return super.scan(e);
+				}
+				return this;
+			}
+		};
+
+		print.scan(element);
+		return print.getResult();
 	}
 
 	/** returns the changed (inserded/deleted/updated) element */
