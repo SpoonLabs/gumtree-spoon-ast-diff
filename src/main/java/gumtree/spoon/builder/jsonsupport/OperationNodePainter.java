dea8ade1b3f3452c1ae8a119d8a249db3f901809
Matias Martinez <martinezmatias@users.noreply.github.com>
2018-10-20 09:20:34 +0200

    Tree painting nodes (#54)
    * new version
    
    * Adding an extensible mechanism to paint a tree
    
    * SNAPSHOT version

---
+++ src/main/java/gumtree/spoon/builder/jsonsupport/OperationNodePainter.java
@@ -0,0 +1,40 @@
+package gumtree.spoon.builder.jsonsupport;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.tree.ITree;
+import com.google.gson.JsonObject;
+
+import gumtree.spoon.builder.Json4SpoonGenerator.JSON_PROPERTIES;
+import gumtree.spoon.diff.operations.Operation;
+
+/**
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class OperationNodePainter implements NodePainter {
+
+	private Map<ITree, Operation> nodesAffectedByOps = new HashMap<>();
+
+	public OperationNodePainter(ITree tree, List<Operation> operations) {
+		// Collect all nodes and get the operator
+		for (Operation operation : operations) {
+			nodesAffectedByOps.put(operation.getAction().getNode(), operation);
+		}
+	}
+
+	@Override
+	public void paint(ITree tree, JsonObject jsontree) {
+
+		if (nodesAffectedByOps.containsKey(tree)) {
+
+			Operation<Action> operationOverTree = nodesAffectedByOps.get(tree);
+			jsontree.addProperty(JSON_PROPERTIES.op.toString(), operationOverTree.getAction().getName());
+		}
+	}
+
+}
