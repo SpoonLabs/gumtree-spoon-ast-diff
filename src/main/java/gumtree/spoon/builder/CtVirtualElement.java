81a770cf7e7deb82197f812a441cb7eee7149b79
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-04-05 11:15:14 +0200

    Bugfix issue #84. We add Nodes related to Itree that represent elements not represented in the Spoon model (such as enumeration).

---
+++ src/main/java/gumtree/spoon/builder/CtVirtualElement.java
@@ -0,0 +1,49 @@
+package gumtree.spoon.builder;
+
+import java.util.Collection;
+
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.path.CtRole;
+
+/**
+ * Represents a virtual node that groups other CtElements in a tree format,
+ * being this object the root of that three. It's an special case of Wrap: here
+ * we wrap a string that describes the virtual node
+ * 
+ */
+public class CtVirtualElement extends CtWrapper<String> {
+
+	protected Collection<?> children;
+
+	public CtVirtualElement(String wrapped, CtElement parent, Collection<?> children) {
+		super(wrapped, parent);
+		this.children = children;
+	}
+
+	public CtVirtualElement(String wrapped, CtElement parent, Collection<?> children, CtRole roleInParent) {
+		super(wrapped, parent, roleInParent);
+		this.children = children;
+	}
+
+	@Override
+	public String toString() {
+		return "Virtual Element " + ((value != null) ? ("with label: " + value.toString()) : null);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (!(o instanceof CtVirtualElement)) {
+			return false;
+		}
+		if (this == o) {
+			return true;
+		}
+		CtVirtualElement anotherVirtual = (CtVirtualElement) o;
+		if (this.value == null && anotherVirtual.value == null)
+			return true;
+
+		return (this.value != null) && this.value.equals(anotherVirtual.value);
+	}
+
+}
