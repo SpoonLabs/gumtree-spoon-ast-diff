baf73d8905290a7dac678f7337d4241fef042e22
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-08-02 15:37:25 +0200

    Fixes issue #108. Replacing lists by sets.

--- src/test/java/gumtree/spoon/diff/DiffTest.java
+++ src/test/java/gumtree/spoon/diff/DiffTest.java
@@ -1,19 +1,23 @@
 package gumtree.spoon.diff;
 
-import gumtree.spoon.AstComparator;
-import gumtree.spoon.diff.operations.Operation;
-import gumtree.spoon.diff.operations.OperationKind;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import gumtree.spoon.AstComparator;
+import gumtree.spoon.diff.operations.Operation;
+import gumtree.spoon.diff.operations.OperationKind;
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
 
 /**
  * Test Spoon Diff
- * @author  Matias Martinez, matias.martinez@inria.fr
+ * 
+ * @author Matias Martinez, matias.martinez@inria.fr
  *
  */
 public class DiffTest {
@@ -25,45 +29,45 @@ public class DiffTest {
 		File fr = new File("src/test/resources/examples/test1/TypeHandler2.java");
 
 		AstComparator diff = new AstComparator();
-		Diff result = diff.compare(fl,fr);
-		assertEquals("Update FieldRead at org.apache.commons.cli.TypeHandler:80" +newline
-				+ "\torg.apache.commons.cli.PatternOptionBuilder.DATE_VALUE to org.apache.commons.cli.PatternOptionBuilder.CLASS_VALUE" + newline
-				+ "Insert Invocation at org.apache.commons.cli.TypeHandler:118" + newline
+		Diff result = diff.compare(fl, fr);
+		assertEquals("Update FieldRead at org.apache.commons.cli.TypeHandler:80" + newline
+				+ "\torg.apache.commons.cli.PatternOptionBuilder.DATE_VALUE to org.apache.commons.cli.PatternOptionBuilder.CLASS_VALUE"
+				+ newline + "Insert Invocation at org.apache.commons.cli.TypeHandler:118" + newline
 				+ "\tjava.lang.System.out.println(\"Hola\")" + newline, result.toString());
 		fl = new File("src/test/resources/examples/test2/CommandLine1.java");
 		fr = new File("src/test/resources/examples/test2/CommandLine2.java");
 
-		result = diff.compare(fl,fr);
-		assertEquals("Update Literal at org.apache.commons.cli.CommandLine:275" + newline
-				+ "\t1 to 1000000" + newline, result.toString());
+		result = diff.compare(fl, fr);
+		assertEquals("Update Literal at org.apache.commons.cli.CommandLine:275" + newline + "\t1 to 1000000" + newline,
+				result.toString());
 
 		fl = new File("src/test/resources/examples/test3/CommandLine1.java");
 		fr = new File("src/test/resources/examples/test3/CommandLine2.java");
 
-		result = diff.compare(fl,fr);
+		result = diff.compare(fl, fr);
 		String deleteStr = "Delete Method at org.apache.commons.cli.CommandLine:161" + newline
 				+ "\tpublic java.lang.String[] getOptionValues(java.lang.String opt) {" + newline
-				+ "\t    java.util.List<java.lang.String> values = new java.util.ArrayList<java.lang.String>();" + newline
-				+ "\t    for (org.apache.commons.cli.Option option : options) {" + newline
+				+ "\t    java.util.List<java.lang.String> values = new java.util.ArrayList<java.lang.String>();"
+				+ newline + "\t    for (org.apache.commons.cli.Option option : options) {" + newline
 				+ "\t        if (opt.equals(option.getOpt()) || opt.equals(option.getLongOpt())) {" + newline
-				+ "\t            values.addAll(option.getValuesList());" + newline
-				+ "\t        }" + newline
-				+ "\t    }" + newline
-				+ "\t    return values.isEmpty() ? null : values.toArray(new java.lang.String[values.size()]);" + newline
-				+ "\t}" + newline;
+				+ "\t            values.addAll(option.getValuesList());" + newline + "\t        }" + newline + "\t    }"
+				+ newline
+				+ "\t    return values.isEmpty() ? null : values.toArray(new java.lang.String[values.size()]);"
+				+ newline + "\t}" + newline;
 		assertTrue(result.toString(), result.toString().endsWith(deleteStr));
 	}
 
 	/**
 	 * Add new element + child
+	 * 
 	 * @throws Exception
 	 */
 	@Test
-	public void test_actionClassifier_1() throws Exception{
+	public void test_actionClassifier_1() throws Exception {
 		AstComparator diff = new AstComparator();
 		File fl = new File("src/test/resources/examples/roots/test8/left_QuickNotepad_1.13.java");
 		File fr = new File("src/test/resources/examples/roots/test8/right_QuickNotepad_1.14.java");
-		Diff result = diff.compare(fl,fr);
+		Diff result = diff.compare(fl, fr);
 
 		List<Operation> actionsRoot = result.getRootOperations();
 		assertEquals(1, actionsRoot.size());
@@ -72,15 +76,17 @@ public class DiffTest {
 
 	/**
 	 * changes an element and adds a new child to it
+	 * 
 	 * @throws Exception
 	 */
 	@Test
-	public void test_actionClassifier_2() throws Exception{
+	public void test_actionClassifier_2() throws Exception {
 		AstComparator diff = new AstComparator();
-		// meld src/test/resources/examples/roots/test9/left_QuickNotepad_1.13.java src/test/resources/examples/roots/test9/right_QuickNotepad_1.14.java
+		// meld src/test/resources/examples/roots/test9/left_QuickNotepad_1.13.java
+		// src/test/resources/examples/roots/test9/right_QuickNotepad_1.14.java
 		File fl = new File("src/test/resources/examples/roots/test9/left_QuickNotepad_1.13.java");
 		File fr = new File("src/test/resources/examples/roots/test9/right_QuickNotepad_1.14.java");
-		Diff result = diff.compare(fl,fr);
+		Diff result = diff.compare(fl, fr);
 
 		List<Operation> actionsRoot = result.getRootOperations();
 		assertEquals(2, actionsRoot.size());
@@ -93,14 +99,15 @@ public class DiffTest {
 
 	/**
 	 * Remove element, keep its only child
+	 * 
 	 * @throws Exception
 	 */
 	@Test
-	public void test_actionClassifier_3() throws Exception{
+	public void test_actionClassifier_3() throws Exception {
 		AstComparator diff = new AstComparator();
 		File fl = new File("src/test/resources/examples/roots/test10/left_QuickNotepad_1.13.java");
 		File fr = new File("src/test/resources/examples/roots/test10/right_QuickNotepad_1.14.java");
-		Diff result = diff.compare(fl,fr);
+		Diff result = diff.compare(fl, fr);
 
 		List<Operation> actionsRoot = result.getRootOperations();
 		assertEquals(2, actionsRoot.size());
@@ -111,14 +118,15 @@ public class DiffTest {
 
 	/**
 	 * Removes element with 2 children, keep one child, remove the other
+	 * 
 	 * @throws Exception
 	 */
 	@Test
-	public void test_actionClassifier_4() throws Exception{
+	public void test_actionClassifier_4() throws Exception {
 		AstComparator diff = new AstComparator();
 		File fl = new File("src/test/resources/examples/roots/test11/left_QuickNotepad_1.13.java");
 		File fr = new File("src/test/resources/examples/roots/test11/right_QuickNotepad_1.14.java");
-		Diff result = diff.compare(fl,fr);
+		Diff result = diff.compare(fl, fr);
 
 		List<Operation> actionsRoot = result.getRootOperations();
 		assertEquals(2, actionsRoot.size());
@@ -129,14 +137,15 @@ public class DiffTest {
 
 	/**
 	 * Add element with 2 children, one new, other from a move
+	 * 
 	 * @throws Exception
 	 */
 	@Test
-	public void test_actionClassifier_5() throws Exception{
+	public void test_actionClassifier_5() throws Exception {
 		AstComparator diff = new AstComparator();
 		File fl = new File("src/test/resources/examples/roots/test12/left_QuickNotepad_1.13.java");
 		File fr = new File("src/test/resources/examples/roots/test12/right_QuickNotepad_1.14.java");
-		Diff result = diff.compare(fl,fr);
+		Diff result = diff.compare(fl, fr);
 
 		List<Operation> actionsRoot = result.getRootOperations();
 		assertEquals(2, actionsRoot.size());
@@ -144,4 +153,46 @@ public class DiffTest {
 		assertTrue(result.containsOperation(OperationKind.Insert, "If"));
 		assertTrue(result.containsOperation(OperationKind.Move, "Invocation"));
 	}
+
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void test_action_RootOrder() throws Exception {
+		AstComparator diff = new AstComparator();
+
+		CtClass c1b = Launcher.parseClass(" class BehaviorCall implements Call{\n"
+				+ "final AtomicReference failureRef = new AtomicReference<>();\n"
+				+ "final CountDownLatch latch = new CountDownLatch(1);\n" + "\n" + " enqueue(new Callback<T>() {\n"
+				+ "  @Override public void onResponse(Response<T> response) {\n" + "     responseRef.set(response);\n"
+				+ "     latch.countDown();\n" + "   }\n" + "}\n" + ")\n" + "\n" + "}");
+
+		CtClass c2b = Launcher.parseClass("class BehaviorCall implements Call {\n"
+				+ "final AtomicReference failureRef = new AtomicReference<>();\n"
+				+ "final CountDownLatch latch = new CountDownLatch(1);\n" + "\n" + " enqueue(new Callback<T>() {\n"
+				+ "@override public void onResponse(Response response) {\n"
+
+				// Added
+				+ "System.out.println();"
+				//
+				+ "responseRef.set(response);\n"
+				// Diff
+				+ "if(response != null){return response;}" + "latch.countDown();\n" + "}\n" + "}\n" + ")\n" + "}");
+
+		Diff result = diff.compare(c1b, c2b);
+
+		List<Operation> actionsRoot = result.getRootOperations();
+		// Buggy
+		actionsRoot = result.getRootOperations();
+		assertEquals(2, actionsRoot.size());
+
+		Operation oproot0 = result.getRootOperations().get(0);
+		Operation oproot1 = result.getRootOperations().get(1);
+
+		int indexInAll0 = result.getAllOperations().indexOf(oproot0);
+		int indexInAll1 = result.getAllOperations().indexOf(oproot1);
+
+		// Preserve the position
+		assertTrue(indexInAll0 < indexInAll1);
+
+	}
+
 }
