ef48f70260124a44440d26062e3f0fc04b317a1d
Aman Sharma <mannu.poski10@gmail.com>
2021-09-27 19:23:46 +0530

    Remove signing of certificate

--- .buildscript/deploy_to_maven.sh
+++ .buildscript/deploy_to_maven.sh
@@ -7,9 +7,6 @@
 set -e
 
 echo "Deploying ..."
-# made with "travis encrypt-file .buildscript/codesigning.asc -r SpoonLabs/gumtree-spoon-ast-diff --add"
-openssl aes-256-cbc -K $encrypted_9809c3ea697e_key -iv $encrypted_9809c3ea697e_iv -in .buildscript/codesigning.asc.enc -out codesigning.asc -d
-gpg --fast-import codesigning.asc
 
 # getting the previous version on Maven Central
 # for some reasons, some versions don't get index by search.maven.org/solrsearch/, and it break the build
