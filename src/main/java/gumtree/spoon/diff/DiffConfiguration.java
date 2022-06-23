57897ef69648c66339fed6d3ecec93576c25f24c
Aman Sharma <mannu.poski10@gmail.com>
2022-06-23 13:12:50 +0200

    Update docstring

--- src/main/java/gumtree/spoon/diff/DiffConfiguration.java
+++ src/main/java/gumtree/spoon/diff/DiffConfiguration.java
@@ -3,8 +3,10 @@ package gumtree.spoon.diff;
 import com.github.gumtreediff.matchers.Matcher;
 
 /**
- * Stores configuration to compute Diff. This is different from {@link com.github.gumtreediff.matchers.GumtreeProperties}
- * as the latter stores the matching configuration. In other words, it stores parameters on which nodes of AST trees are matched.
+ * Stores configuration to compute Diff. This is different from {@link com.github.gumtreediff.matchers.GumtreeProperties}.
+ * `DiffConfiguration` stores properties which are more general in nature and are not specific to matchers.
+ *
+ * In the future, this class can also be wrapper to store all types of configuration, whether required by gumtree or gumtree-spoon.
  */
 public class DiffConfiguration {
     private Matcher matcher;
